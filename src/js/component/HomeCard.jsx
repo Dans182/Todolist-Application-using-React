1d2802d8bf25df6ab314cc61f916e2c7acd8e256
Dans182 <dgaiteiro@gmail.com>
2022-04-06 11:10:57 +0000

    v5

--- src/js/component/HomeCard.jsx
+++ src/js/component/HomeCard.jsx
@@ -2,36 +2,48 @@ import React, { useState } from "react";
 
 const HomeCard = () => {
 	const [task, setTask] = useState("");
+	const [lista, setLista] = useState([]);
 
 	return (
 		<form>
-			<div className="back bg-light mx-auto w-75">
+			<div
+				className="back bg-light mx-auto rounded"
+				style={{ width: "450px" }}>
 				<div className="mb-3 w-50 mx-auto mt-4">
 					<label htmlFor="exampleInputEmail1" className="form-label">
 						To Do List
 					</label>
 					<input
-						type="Task"
 						className="form-control"
-						id="Input-Task"
+						id="exampleInputEmail1"
 						aria-describedby="emailHelp"
 						placeholder="Agrega una tarea"
-						defaultValue={task}
-						// onChange
-						onKeyPress={(e) => {
-							if (e.key === "Enter") {
-								console.log(e.target.value);
-							}
+						value={task}
+						onChange={(e) => {
 							setTask(e.target.value);
+							console.log(e.target.value);
 						}}
 					/>
-					<ul>
-						<li>{task}</li>
-					</ul>
-
-					<div id="emailHelp" className="form-text">
-						We'll never share your email with anyone else.
-					</div>
+				</div>
+				<div>
+					<button
+						type="button"
+						className="btn btn-warning justify-content-center"
+						onClick={() => {
+							setLista([...lista, task]);
+							setTask("");
+						}}>
+						Warning
+					</button>
+					{lista.map((ingres) => {
+						return (
+							<div
+								className="card"
+								style={{ width: "225px", height: "225px" }}>
+								<h4>{ingres}</h4>
+							</div>
+						);
+					})}
 				</div>
 			</div>
 		</form>
