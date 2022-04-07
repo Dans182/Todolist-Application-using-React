93de6dff3e8f1e217a86ec3d72c9fb35d3c8ab9f
Dans182 <dgaiteiro@gmail.com>
2022-04-07 16:48:44 +0000

    v11

--- src/js/component/Header.jsx
+++ src/js/component/Header.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 
 const Header = () => {
 	return (
-		<label className="form-label mt-3">
-			<h3>To Do List</h3>
-		</label>
+		<div className="text-center">
+			<label className="form-label mt-3">
+				<h3>To Do List</h3>
+			</label>
+		</div>
 	);
 };
 
