1d57875e1d61ee12e5822d46a968f61d12e894f1
Daniel <97400328+Dans182@users.noreply.github.com>
2022-04-07 17:09:54 +0200

    Update AddTask.jsx

--- src/js/component/AddTask.jsx
+++ src/js/component/AddTask.jsx
@@ -14,75 +14,64 @@ const AddTask = () => {
 	};
 
 	return (
-		<form>
-			<div
-				className="bg-light mx-auto rounded"
-				style={{ width: "450px" }}>
-				<div className="mb-3 w-50 pb-2 mx-auto mt-4">
-					<label className="form-label mt-3">
-						<h3>To Do List</h3>
-					</label>
-					<input
-						className="form-control"
-						placeholder="Write a task"
-						value={task}
-						onChange={(e) => {
-							setTask(e.target.value);
-							console.log(e.target.value);
-						}}
-						onKeyPress={(e) => {
-							if (e.key === "Enter") {
-								e.preventDefault();
-								e.stopPropagation();
-								// setTaskList([...taskList, task]);
-								nuevaTarea();
-								setTask("");
-							}
-						}}
-					/>
-					<button
-						type="button"
-						className="btn btn-warning mt-3 mb-3"
-						onClick={() => {
+		<div>
+			<form>
+				<input
+					className="form-control"
+					placeholder="Write a task"
+					value={task}
+					onChange={(e) => {
+						setTask(e.currentTarget.value);
+						console.log(e.currentTarget.value);
+					}}
+					onKeyPress={(e) => {
+						if (e.key === "Enter") {
+							e.preventDefault();
+							e.stopPropagation();
 							// setTaskList([...taskList, task]);
 							nuevaTarea();
 							setTask("");
-						}}>
-						Add a task
-					</button>
+						}
+					}}
+				/>
+			</form>
+			<button
+				type="button"
+				className="btn btn-warning mt-3 mb-3"
+				onClick={() => {
+					// setTaskList([...taskList, task]);
+					nuevaTarea();
+					setTask("");
+				}}>
+				Add a task
+			</button>
 
-					{/* MOSTRAR TAREA ANOTADA */}
-
-					{taskList.map((tareaAlmacenada, index) => {
-						return (
-							<div
-								key={index}
-								className="bg-light"
-								style={{ width: "225px" }}>
-								<h4>
-									<input
-										type="checkbox"
-										className={
-											"checkbox" == true ? "strike" : ""
-										}
-									/>{" "}
-									{tareaAlmacenada}{" "}
-									<i
-										className="fas fa-times-circle"
-										type="button"
-										onClick={() => {
-											eliminarTarea(index);
-										}}></i>
-								</h4>
-							</div>
-						);
-					})}
-
-					{/* ELIMINAR TAREA ANOTADA */}
-				</div>
+			{/* MOSTRAR TAREA ANOTADA */}
+			<div>
+				{taskList.map((tareaAlmacenada, index) => {
+					return (
+						<div
+							key={index}
+							className="bg-light"
+							style={{ width: "225px" }}>
+							<h4>
+								<input type="checkbox" /> {tareaAlmacenada}{" "}
+								<i
+									className="fas fa-times-circle"
+									type="button"
+									onClick={() => {
+										eliminarTarea(index);
+									}}></i>
+							</h4>
+						</div>
+					);
+				})}
 			</div>
-		</form>
+		</div>
 	);
+	{
+		/* ELIMINAR TAREA ANOTADA */
+	}
 };
 
 export default AddTask;
