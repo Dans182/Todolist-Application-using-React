d33fd2ad961978a8d7a5e0488308fc11b954d73a
Daniel <97400328+Dans182@users.noreply.github.com>
2022-04-07 17:10:33 +0200

    Create Header.jsx

---
+++ src/js/component/Header.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Header = () => {
+	return (
+		<label className="form-label mt-3">
+			<h3>To Do List</h3>
+		</label>
+	);
+};
+
+export default Header;
